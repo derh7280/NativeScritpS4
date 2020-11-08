c2e261b757efe0fec76a993d26ef871b24b24f3c
Duhan Renteria <derh7280@hotmail.com>
2020-11-08 15:13:08 -0500

    Persistencia en almacenamiento NoSQL con Couchbase Lite

--- my-drawer-ng/src/app/domain/noticias.service.ts
+++ my-drawer-ng/src/app/domain/noticias.service.ts
@@ -1,66 +1,7 @@
-// import { Injectable } from "@angular/core";
-// import { getJSON, request } from "@nativescript/core/http";//"tns-core-modules/http";
-// const sqlite = require("nativescript-sqlite");
-
-// @Injectable()
-// export class NoticiasService    {
-//     api: string ="https://0230b734463a.ngrok.io";
-
-//     constructor(){
-//         this.getDb((db) => {
-//             console.dir(db);
-//             db.each("SELECT * FROM logs",
-//                 (err, fila) => console.log("fila: ", fila),
-//                 (err, totales) => console.log("Filas totales: ", totales));
-//             }, () => console.log("error on getDB"));
-//     }
-
-//     getDb(fnOk, fnError) {
-//         return new sqlite("mi_db_logs", (err, db) => {
-//             if(err) {
-//                 console.error("Error al abrir db!", err);
-//             } else {
-//                 console.log("Está la db abierta: ", db.isOpen() ? "Sí" : "No");
-//                 db.execSQL("CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, texto TEXT)")
-//                     .then((id) => {
-//                         console.log("CREATE TABLE OK");
-//                         fnOk(db);
-//                     }, (error) => {
-//                         console.log("CREATE TABLE ERROR", error);
-//                         fnError(error);
-//                     });
-//             }
-//         });
-//     }
-
-//     agregar(s: string)  {
-//         return request({
-//             url: this.api + "/favs",
-//             method: "POST",
-//             headers: {"Content-Type" : "application/json"},
-//             content: JSON.stringify({
-//                 nuevo:s
-//             })
-//         });
-//     }
-
-//     favs(){
-//         return getJSON(this.api + "/favs");
-//     }
-
-//     buscar(s: string) {
-//         this.getDb((db) => {
-//             db.execSQL("INSERT INTO logs(texto) VALUES (?)", [s], 
-//                 (err, id) => console.log("nuevo id: ", id));
-//         }, () => console.log("error on getDB"));
-        
-//         return getJSON(this.api + "/get?q=" +s);
-//     } 
-
-// }
-
 import { Injectable } from "@angular/core";
-import { getJSON, request } from "@nativescript/core/http";//"tns-core-modules/http";
+import * as couchbaseModule from "nativescript-couchbase"; 
+// import couchbaseModule = require("nativescript-couchbase"); 
+import { getJSON, request } from "@nativescript/core/http";
 const sqlite = require("nativescript-sqlite");
 
 @Injectable()
@@ -68,9 +9,11 @@ export class NoticiasService    {
     api: string ="https://0230b734463a.ngrok.io";
     private database: any;//instancia SQLite abierta 
     public noticiero: Array<any>;//los resultados de las consulta.
+    databaseDoc: couchbaseModule.Couchbase; 
 
     constructor(){
         // this.CreateDB("mi_db_logs");
+        this.databaseDoc = new couchbaseModule.Couchbase("test-database"); 
         this.getDB((db) => {
             console.dir(db);
             db.each("SELECT * FROM logs",
@@ -78,6 +21,10 @@ export class NoticiasService    {
                 (err, totales) => console.log("Filas totales: ", totales));
         }, () => console.log("error en getDB"));
         this.CreateTable("mi_db_logs","favoritos","id INTEGER PRIMARY KEY AUTOINCREMENT, texto TEXT");
+        this.databaseDoc.createView("logs", "1", (document, emitter) =>
+        emitter.emit(document._id, document));
+        const rows = this.database.executeQuery("logs", {limit : 200});
+        console.log("documentos: " + JSON.stringify(rows)); 
     }
 
     CreateDB(DBname) {
@@ -168,6 +115,9 @@ export class NoticiasService    {
                 (err, id) => console.log("nuevo id: ", id));
         }, () => console.log("error on getDB"));
         
+        const documentId = this.databaseDoc.createDocument({ texto: s });
+        console.log("nuevo id couchbase: ", documentId); 
+
         return getJSON(this.api + "/get?q=" +s);
     } 
 
